a7950cca73d529885f899a18b51df56f3bbbb676
hp <uk129195@gmail.com>
2024-11-09 13:07:47 +0500

    about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,6 @@
 const about = () => {
   return (
-    <div className="flex flex-col my-20 gap-4 font-sans px-4 lg:px-36 justify-center items-center h-[min-450px] py-5">
+    <div className="flex flex-col my-20 gap-4 font-sans px-4 md:px-12 lg:px-36 justify-center items-center h-[min-450px] py-5">
     <h3 className="text-[#fcb532] font-semibold text-2xl">My Intro</h3>
     <h2 className="text-3xl font-bold">About Me</h2>
     <p>My expertise extends to various design tools, including Photoshop, Illustrator, and Figma. With these powerful tools, I create immersive and experiential projects that not only impress clients but also elevate brands to new heights by providing exceptional user experiences.</p>
