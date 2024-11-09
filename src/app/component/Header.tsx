4fb327e35045b24305078cc6ac8ebf662ba176e0
hp <uk129195@gmail.com>
2024-11-09 13:30:26 +0500

    new

--- src/app/component/Header.tsx
+++ src/app/component/Header.tsx
@@ -12,7 +12,7 @@ const Header = () => {
         <div className="flex bg-[#f2f2f3]  h-28  gap-7 px-4 py-3">
             {/* logo */}
             <Image src={"/logo (1).png"} alt={"logo"} width={100} height={200}></Image>
-            <ul className="flex gap lg:gap-14 text-base lg:text-lg font-sans m-auto items-center justify-center ">
+            <ul className="flex gap-4 lg:gap-14 text-base lg:text-lg font-sans m-auto items-center justify-center ">
                 <li><Link href="/">HOME</Link></li>
                 <li><Link href="/about">ABOUT</Link></li>
                 <li><Link href="/contact">CONTACT</Link></li>
